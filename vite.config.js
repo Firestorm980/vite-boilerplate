c6072e5b890d033d09ec138208b9f2cd80876a64
Jon Christensen <jon.christensen@get10up.com>
2023-01-31 08:21:52 -0600

    Move assets and public files to closer align boilerplates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
 
-export default defineConfig({
-  plugins: [eslint(), stylelint()]
+export default defineConfig(({ command, mode }) => {
+  return {
+    root: 'src',
+    plugins: [eslint(), stylelint()]
+  }
 })
